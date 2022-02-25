e645701af9cc0f3c7b30c343e009ff229fd31612
willyc <willcowendrums@gmail.com>
2022-02-25 17:23:34 +0000

    progress on ext 1

--- src/App.js
+++ src/App.js
@@ -1,44 +1,76 @@
-import { useState } from "react"
-import {initialWorkouts, generateWorkout} from "./Workouts.js"
-import "./App.css"
+import { useState } from "react";
+import { initialWorkouts, generateWorkout } from "./Workouts.js";
+import "./App.css";
 
 function App() {
-  const [workouts, setWorkouts] = useState(initialWorkouts)
-
+  const [workouts, setWorkouts] = useState(initialWorkouts);
+  const [checked, setChecked] = useState(true)
   const addNewWorkout = () => {
-    const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
-  }
+    const newWorkout = generateWorkout();
+    setWorkouts([...workouts, newWorkout]);
+  };
 
-  const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
-  }
+  const deleteWorkout = (target) => {
+    const workoutsWithoutDeleted = workouts.filter(function (workout) {
+      return workout !== target;
+    });
+    setWorkouts(workoutsWithoutDeleted);
+  };
 
-  const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
+  const completeWorkout = (target) => {
+    const completedWorkouts = workouts.map(function (workout) {
+      if (workout === target) {
+        return { ...workout, done: (workout.done = true) };
+      }
+
+      return workout;
+    });
+    setWorkouts(completedWorkouts);
+  };
+
+  const handleChange = () => {
+    const allworkouts = [...workouts]
+    const completedWorkouts = workouts.filter(workout => workout.done)
+    if(!checked) {
+    setWorkouts(allworkouts)
+    setChecked(true)
+    
+    }
+    else {
+      setWorkouts(completedWorkouts)
+      setChecked(false)
+    }
+    console.log(checked)
   }
 
   return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
       <button onClick={addNewWorkout}>Add New Workout</button>
+      <div>
+        <input type="checkbox" name="toggle" onChange={handleChange}/>
+        <label for="toggle">Show Done Only</label>
+      </div>
       <ul>
         {workouts.map((workout, index) => (
           <li key={index}>
             <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
+              {workout.sets}x sets of{" "}
+              <strong>
+                {workout.reps}x{workout.exercise}
+              </strong>{" "}
+              with {workout.rest} seconds rest
             </p>
-            {!workout.done && 
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done && 
-             <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
+            {!workout.done && (
+              <button onClick={(e) => completeWorkout(workout)}>Done</button>
+            )}
+            {workout.done && <p>✅</p>}
+            <button onClick={(e) => deleteWorkout(workout)}>Delete</button>
           </li>
         ))}
       </ul>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
